a6b305bfca18132bc7367c6dcd39dcb176444678
Nicholas Curran <nicholas.a.curran@gmail.com>
2015-04-28 16:41:19 -0400

    new backdoor

--- qemu.sh
+++ qemu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-system-mipsel -kernel $1 -nographic -m 256 -net tap,ifname=tap0,script=no,downscript=no -net nic,model=e1000
+qemu-system-mipsel -kernel $1 -nographic -m 512 -net tap,ifname=tap0,script=no,downscript=no -net nic,model=e1000
